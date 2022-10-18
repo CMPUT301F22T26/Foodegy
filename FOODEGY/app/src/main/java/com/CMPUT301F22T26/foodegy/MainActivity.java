791473c582a43305baa6e61157717922b0b09582
enortho <eopgenor@ualberta.ca>
2022-10-17 19:07:17 -0600

    added Firebase & Junit to the gradle

--- FOODEGY/app/src/main/java/com/CMPUT301F22T26/foodegy/MainActivity.java
+++ FOODEGY/app/src/main/java/com/CMPUT301F22T26/foodegy/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
