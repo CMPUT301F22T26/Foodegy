612dd937067dc2f2309233b4e36a96d472f7357d
Raphael Gutierrez <93965834+rmgutierrez@users.noreply.github.com>
2022-10-17 19:07:23 -0600

    rough AddRecipe
    Added (a non-functional) AddRecipeActivity class for when user wants to add a recipe, as well as the xml file for the layout. Added miscellaneous files such as edit_text_border.xml for the editText white borders, added some colors to colors.xml.
    **Buttons and editTexts are not yet defined in AddRecipeActivity class**

--- FOODEGY/app/src/main/java/com/CMPUT301F22T26/foodegy/AddRecipeActivity.java
+++ FOODEGY/app/src/main/java/com/CMPUT301F22T26/foodegy/AddRecipeActivity.java
@@ -1,15 +1,15 @@
 package com.CMPUT301F22T26.foodegy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class AddRecipeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.add_recipe_activity);
+
     }
 }
