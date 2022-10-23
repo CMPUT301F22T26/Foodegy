b06fb69ccc15b028489326fca1b1ca737dca52dd
Gurkirat Singh <gsingh8@ualberta.ca>
2022-10-23 17:06:36 -0600

    added mealPlanItem class

---
+++ FOODEGY/app/src/main/java/com/CMPUT301F22T26/foodegy/MealPlanItem.java
@@ -0,0 +1,86 @@
+package com.CMPUT301F22T26.foodegy;
+
+public class MealPlanItem {
+    /***
+     * this is a class which defines a single item constituting a meal plan, a string type is also
+     * defined to show that the item is either an ingredient or a recipe.
+     * These all items will be stored in a single list and they will be shown according to the
+     * date selected from calendar.
+     * Here both a 'StorageIngredient' and 'Recipe' object is defined in each item. While instantiation,
+     * if item is StorageIngredient, 'StorageIngredient' object is initialised and 'Recipe' object is
+     * initialised to a null value (vice versa for 'Recipe' item).
+     */
+    private int date;
+    private int month;
+    private int year;
+    private String type;
+    private StorageIngredient ingredient;
+    private Recipe mealRecipe;
+    private int servings;
+
+    public MealPlanItem(int date, int month, int year, String type, StorageIngredient ingredient, Recipe mealRecipe, int servings){
+        this.date = date;
+        this.month = month;
+        this.year = year;
+        this.type = type;
+        this.ingredient = ingredient;
+        this.mealRecipe = mealRecipe;
+        this.servings = servings;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public StorageIngredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(StorageIngredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Recipe getMealRecipe() {
+        return mealRecipe;
+    }
+
+    public void setMealRecipe(Recipe mealRecipe) {
+        this.mealRecipe = mealRecipe;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+}
