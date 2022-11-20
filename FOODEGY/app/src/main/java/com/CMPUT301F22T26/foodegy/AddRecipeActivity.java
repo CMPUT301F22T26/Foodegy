c39d4cf383326bd782e748c2472210dd8d283cf6
TarushShankar <tarush@ualberta.ca>
2022-11-19 18:09:50 -0700

    EditRecipeActivity.java gets the data from ViewRecipeActivity.java. The data transfer works

--- FOODEGY/app/src/main/java/com/CMPUT301F22T26/foodegy/AddRecipeActivity.java
+++ FOODEGY/app/src/main/java/com/CMPUT301F22T26/foodegy/AddRecipeActivity.java
@@ -56,7 +56,7 @@ public class AddRecipeActivity extends AppCompatActivity implements AddIngredien
     private Spinner categorySpinner;
 
     // Ingredients list to be used by quick add ingredients and added to recipe
-    public ArrayList<RecipeIngredient> ingredientsList;
+    public static ArrayList<RecipeIngredient> ingredientsList;
     private ListView ingredientsListView;
     private RecipeIngredientListAdapter ingredientsAdapter;
 
